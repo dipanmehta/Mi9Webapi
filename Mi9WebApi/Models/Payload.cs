edfd45ba1c83510b543cd9936a50693d452a8d88
dipanmehta <dipanvmehta@gmail.com>
2014-08-14 12:32:57 +1000

    Update Payload.cs
    remove Json Serialisation and make it work by existing framework

--- Mi9WebApi/Models/Payload.cs
+++ Mi9WebApi/Models/Payload.cs
@@ -5,19 +5,18 @@ using System.Web;
 using Newtonsoft.Json;
 namespace Mi9Project.Models
 {
-    [JsonObject(MemberSerialization.OptIn)]
     public class Payload
     {
-        [JsonProperty(PropertyName="payload")]
+    
         public List<Show> payload { get; set; }
 
-        [JsonProperty(PropertyName="skip")]
+    
         public int skip { get; set; }
 
-        [JsonProperty(PropertyName="take")]
+    
         public int take { get; set; }
 
-        [JsonProperty(PropertyName="totalRecords")]
+    
         public int totalRecords { get; set; }
 
     }
